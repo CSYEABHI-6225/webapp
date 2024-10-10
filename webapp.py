c2131c8514b467da7b7ef21286ff8448b190859a
Abhishek Singh <singh.abhishek3@northeastern.edu>
2024-10-10 02:16:54 -0400

    ci(updating files):updating workflows

--- webapp.py
+++ webapp.py
@@ -3,7 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
-from dotenv import load_dotenv
 import re
 import os
 from sqlalchemy import text
@@ -16,7 +15,7 @@ print(os.getenv('SQLALCHEMY_DATABASE_URI'))
 # Configure the database connection using SQLAlchemy
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['HOSTNAME'] = '0.0.0.0'
+app.config['HOSTNAME'] = os.getenv('HOSTNAME')
 
 db = SQLAlchemy(app)
 auth = HTTPBasicAuth()
