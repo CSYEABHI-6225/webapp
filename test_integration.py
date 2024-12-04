e0e323c727e0f1b5080f24212e1db397a6e5d1f4
Abhishek Singh <singh.abhishek3@northeastern.edu>
2024-12-04 17:23:07 -0500

    cicds

--- test_integration.py
+++ test_integration.py
@@ -112,6 +112,10 @@ def test_health_check(client):
     response = client.get('/healthz')
     assert response.status_code == 200
 
+def test_health_check(client):
+    response = client.get('/cicd')
+    assert response.status_code == 200
+
 def test_create_user(client, mock_aws):
     data = {
         "first_name": "John",
